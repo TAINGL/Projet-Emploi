41f1d1c86b2e7575007e5f03ed9fa7241e28192b
Laura Taing <laura.taing@hotmail.com>
2020-09-24 17:27:41 +0200

    mongo-flask

--- application/app.py
+++ application/app.py
@@ -1,113 +1,34 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
 import os
 import json
 import re
 from flask import Flask, render_template, url_for, redirect, flash, request, jsonify
-from flask_wtf import FlaskForm
+from flask import jsonify
+from flask import request
 from wtforms import StringField, SubmitField
 from flask_pymongo import PyMongo
-from pymongo import MongoClient
+import pymongo 
+from bson.json_util import dumps
 
-# Configure the Flask application to connect with the MongoDB server
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/job_applications"
+
 app.config['MONGO_DBNAME'] = 'Indeed'
-#app.config['SECRET_KEY'] = 'secret_key'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/job_applications'
 
-# Connect to MongoDB using Flask's PyMongo wrapper
 mongo = PyMongo(app)
-db = mongo.db
-col = mongo.db["Indeed"]
-print ("MongoDB Database:", mongo.db)
+job_collection = mongo.db.Indeed
 
-# Declare an app function that will return some HTML
-@app.route("/mongo")
-def connect_mongo():
-    pass
+@app.route("/", methods=['POST'])    
+def index():
+    query = request.json['q']
+    sort = request.json['s']
 
-
-@app.route('/', methods=['GET', 'POST'])
-def home_page():
-    key=request.values.get("key")
-    key=request.values.get("key")
-    refer=request.values.get("refer")
-    refer_plus=request.values.get("refer_plus")
+    job_collection = mongo.db.Indeed
     
-    if (key=="Title"):
-        mydoc = col.find({refer: { "$regex": key} })
-    elif (key=="Skills"):
-        mydoc = col.find({refer:{ "$all": [key]} })
-    elif (key=="Company"):
-        mydoc = col.find({refer:key})
-    elif (key=="Location"):
-        mydoc = col.find({refer:{ "$regex": key} })
-    elif (key=="Location" & refer_plus="Skills"):
-        mydoc = col.find({refer: { "$in": [key] },
-                          refer_plus: { "$regex": key} })
-	else:
-        mydoc = col.find({refer:key})
-    for x in mydoc:
-        print(x)     
-    return render_template('index.html')
-
-@app.route('/results', methods=['GET', 'POST'])
-def get_results():
-    return render_template('offer.html')
-
-""" @app.route('/results')
-def get_results(query):
-    offer = mongo.db.data.find_one_or_404({"_id": offer_id})
-    return render_template('offer.html', title=offer.title, offer=offer)
-
-@app.route('/offer/<int:offer_id>')
-def show_offer(offer_id):
-    offer = mongo.db.data.find_one_or_404({"_id": offer_id})
-    return render_template('offer.html', title=offer.title, offer=offer)
- """
-
-""" class User(db.Document):
-    name = db.StringField()
-    email = db.StringField()
-    def to_json(self):
-        return {"name": self.name,
-                "email": self.email}
-
-@app.route('/', methods=['GET'])
-def query_records():
-    name = request.args.get('name')
-    user = User.objects(name=name).first()
-    if not user:
-        return jsonify({'error': 'data not found'})
+    if sort == 'asc':
+        result = job_collection.find(query)
     else:
-        return jsonify(user.to_json())
+        result = job_collection.find(query).sort('index', pymongo.DESCENDING)
+    return dumps(result)
 
-@app.route('/', methods=['PUT'])
-def create_record():
-    record = json.loads(request.data)
-    user = User(name=record['name'],
-                email=record['email'])
-    user.save()
-    return jsonify(user.to_json())
-
-@app.route('/', methods=['POST'])
-def update_record():
-    record = json.loads(request.data)
-    user = User.objects(name=record['name']).first()
-    if not user:
-        return jsonify({'error': 'data not found'})
-    else:
-        user.update(email=record['email'])
-    return jsonify(user.to_json())
-
-@app.route('/', methods=['DELETE'])
-def delete_record():
-    record = json.loads(request.data)
-    user = User.objects(name=record['name']).first()
-    if not user:
-        return jsonify({'error': 'data not found'}) """
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
--- application/mongo.py
+++ application/mongo.py
@@ -4,8 +4,7 @@ import os
 import json
 import re
 from flask import Flask, render_template, url_for, redirect, flash, request, jsonify
-from flask import jsonify
-from flask import request
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from flask_pymongo import PyMongo
 import pymongo 
@@ -15,23 +14,52 @@ app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'Indeed'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/job_applications'
+app.config["SECRET_KEY"] = '296e3a31e0fe30ddf14efa247e70b56b' # python :> import secrets > secrets.token_hek(16)
 
 mongo = PyMongo(app)
 job_collection = mongo.db.Indeed
 
-@app.route("/", methods=['POST'])    
-def index():
-    query = request.json['q']
-    sort = request.json['s']
+class SearchForm(FlaskForm):
+    query = StringField('What Skills do you have?')
+    city = StringField('Where?')
+    company = StringField('Which company?')
+    submit = SubmitField('Submit')
 
+@app.route("/")
+@app.route("/search", methods=['GET', 'POST'])    
+def index():
+    form = SearchForm()
+    if request.method == "POST" and form.validate_on_submit():
+        keywords = form.query.data if hasattr(form.query, 'data') else ""
+        city = form.query.city if hasattr(form.query, 'city') else ""
+        company = form.query.company if hasattr(form.query, 'company') else ""
+        return redirect(url_for('get_offers', keywords=keywords, city=city, company=company))
+    return render_template('index.html', form=form)
+
+@app.route("/result")    
+def get_offers():
     job_collection = mongo.db.Indeed
-    
-    if sort == 'asc':
-        result = job_collection.find(query)
+
+    keywords = request.args.get("keywords")
+    city = request.args.get("city")
+    company = request.args.get("company")
+
+    myquery = {}
+    if city:
+        myquery = { "Location": { "$regex": city }}
+    elif city and company:
+        myquery = {
+        "Company" : { "$in": list(company) },
+        "Location" : { "$regex": "paris" }
+    }
+    elif keywords:
+        myquery = { "Skills": { "$all": list(keywords)} }
     else:
-        result = job_collection.find(query).sort('index', pymongo.DESCENDING)
-    return dumps(result)
-    #return render_template('index.html')
+        job_collection.find({"exp":10})
+
+
+    results = job_collection.find(myquery)
+    return render_template('offer.html', results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
